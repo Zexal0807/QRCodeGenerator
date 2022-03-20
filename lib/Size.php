ae23688a260017933f67767d7e66eb6b7ed7c478
Zexal0807 <gallinar00@gmail.com>
2022-03-20 11:06:18 +0100

    Add sizes

---
+++ lib/Size.php
@@ -0,0 +1,44 @@
+<?php
+class Size
+{
+    const LEVEL_1 = ["width" => 21, "height" => 21];
+    const LEVEL_2 = ["width" => 25, "height" => 25];
+    const LEVEL_3 = ["width" => 29, "height" => 29];
+    const LEVEL_4 = ["width" => 33, "height" => 33];
+    const LEVEL_5 = ["width" => 37, "height" => 37];
+    const LEVEL_6 = ["width" => 41, "height" => 41];
+    const LEVEL_7 = ["width" => 45, "height" => 45];
+    const LEVEL_8 = ["width" => 49, "height" => 49];
+    const LEVEL_9 = ["width" => 53, "height" => 53];
+    const LEVEL_10 = ["width" => 57, "height" => 57];
+    const LEVEL_11 = ["width" => 61, "height" => 61];
+    const LEVEL_12 = ["width" => 65, "height" => 65];
+    const LEVEL_13 = ["width" => 69, "height" => 69];
+    const LEVEL_14 = ["width" => 73, "height" => 73];
+    const LEVEL_15 = ["width" => 77, "height" => 77];
+    const LEVEL_16 = ["width" => 81, "height" => 81];
+    const LEVEL_17 = ["width" => 85, "height" => 85];
+    const LEVEL_18 = ["width" => 89, "height" => 89];
+    const LEVEL_19 = ["width" => 93, "height" => 93];
+    const LEVEL_20 = ["width" => 97, "height" => 97];
+    const LEVEL_21 = ["width" => 101, "height" => 101];
+    const LEVEL_22 = ["width" => 105, "height" => 105];
+    const LEVEL_23 = ["width" => 109, "height" => 109];
+    const LEVEL_24 = ["width" => 113, "height" => 113];
+    const LEVEL_25 = ["width" => 117, "height" => 117];
+    const LEVEL_26 = ["width" => 121, "height" => 121];
+    const LEVEL_27 = ["width" => 125, "height" => 125];
+    const LEVEL_28 = ["width" => 129, "height" => 129];
+    const LEVEL_29 = ["width" => 133, "height" => 133];
+    const LEVEL_30 = ["width" => 137, "height" => 137];
+    const LEVEL_31 = ["width" => 141, "height" => 141];
+    const LEVEL_32 = ["width" => 145, "height" => 145];
+    const LEVEL_33 = ["width" => 149, "height" => 149];
+    const LEVEL_34 = ["width" => 153, "height" => 153];
+    const LEVEL_35 = ["width" => 157, "height" => 157];
+    const LEVEL_36 = ["width" => 161, "height" => 161];
+    const LEVEL_37 = ["width" => 165, "height" => 165];
+    const LEVEL_38 = ["width" => 169, "height" => 169];
+    const LEVEL_39 = ["width" => 173, "height" => 173];
+    const LEVEL_40 = ["width" => 177, "height" => 177];
+}
