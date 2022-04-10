ec59a607b6525ec820f9537e42fb3067a92bf36f
Zexal0807 <gallinar00@gmail.com>
2022-04-10 18:36:12 +0200

    Add print matrix

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<head>
+    <style>
+        td[color="0"] {
+            background-color: black;
+        }
+
+        td[color="1"] {
+            background-color: white;
+        }
+
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            background-color: #ff0000;
+        }
+
+        table {
+            width: 500px;
+            height: 500px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
 <?php
 
 require_once('./d_var_dump.php');
@@ -14,8 +38,8 @@ $errorCorrection = ErrorCorrection::$CORRECTION_Q;
 
 //$level =  QRCode::findBestLevel($data, $encoding, $errorCorrection);
 
-d_var_dump(QRCode::generate($data, $level, $encoding, $errorCorrection));
-
+$matrix = QRCode::generate($data, $level, $encoding, $errorCorrection);
+echo ($matrix->printMatrix());
 
 
 /*
