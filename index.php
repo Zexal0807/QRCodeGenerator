42271de7cc1cdfb8e91cf16104bdd4512076af62
Zexal0807 <gallinar00@gmail.com>
2022-04-10 12:13:41 +0200

    Add d_var_dump

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
+
+require_once('./d_var_dump.php');
+
+
 require_once('./lib/QRCode.php');
 
 $data = "HELLO WORLD";
 
-$level = Level::$LEVEL_1;
+$level = Level::$LEVEL_5;
 $encoding = Encoding::$ENCODING_ALPHANUMERIC;
 $errorCorrection = ErrorCorrection::$CORRECTION_Q;
 
 
 //$level =  QRCode::findBestLevel($data, $encoding, $errorCorrection);
 
-echo QRCode::generate($data, $level, $encoding, $errorCorrection);
+d_var_dump(QRCode::generate($data, $level, $encoding, $errorCorrection));
+
+
+
+/*
 echo "<br>";
+
 echo "00100000 01011011 00001011 01111000 11010001 01110010 11011100 01001101 01000011 01000000 11101100 00010001 11101100";
+*/
+
 
 //https://www.thonky.com/qr-code-tutorial/structure-final-message
