349217ef42773dcbaf3e90ec3ebe300112b37b86
Zexal0807 <gallinar00@gmail.com>
2022-04-15 12:59:35 +0200

    Return all matrixs

--- lib/QRCode.php
+++ lib/QRCode.php
@@ -2,6 +2,7 @@
 require_once('./lib/loadData.php');
 require_once('./lib/PolynomialDivision.php');
 require_once('./lib/MatrixManager.php');
+require_once('./lib/MaskerMaker.php');
 
 class QRCode
 {
@@ -10,28 +11,26 @@ class QRCode
     {
         $dataCodewords = QRCode::encode($data, $level,  $encoding, $errorCorrection);
 
+
         $dataCodewords = QRCode::splitCodewords($dataCodewords, $level,  $encoding, $errorCorrection);
 
-        /*
+
+        //TODO: DUCK è giusto ma sono invertiti - FUCK
         $dataCodewords = [
             'GROUP_1' => [
                 'BLOCK_1' => [
-                    67, 85, 70, 134, 87, 38, 85, 194, 119, 50, 6, 18, 6, 103, 38
-                ],
-                'BLOCK_2' => [
-                    246, 246, 66, 7, 118, 134, 242, 7, 38, 86, 22, 198, 199, 146, 6
+                    32, 91, 11, 120, 209, 114, 220, 77, 67, 64, 236, 17, 236
                 ]
             ],
             'GROUP_2' => [
-                'BLOCK_1' => [
-                    182, 230, 247, 119, 50, 7, 118, 134, 87, 38, 82, 6, 134, 151, 50, 7
-                ],
-                'BLOCK_2' => [
-                    70, 247, 118, 86, 194, 6, 151, 50, 16, 236, 17, 236, 17, 236, 17, 236
-                ]
+                'BLOCK_1' => [],
+                'BLOCK_2' => []
             ]
-        ];*/
+        ];
+
         $dataCodewords = QRCode::calcErrorCodewords($dataCodewords, $level,  $errorCorrection);
+
+        d_var_dump($dataCodewords);
 
         $data = QRCode::interleavedCodeword($dataCodewords, $level, $errorCorrection);
 
@@ -41,11 +40,12 @@ class QRCode
         $data = implode("", $data);
         $data = $data . str_repeat("0", $level->getRemainderBits());
 
-
         $matrix = new MatrixManager($level);
         $matrix->setData($data);
+        $matrix = $matrix->getMatrix();
 
-        return $matrix;
+        $matrixs = MaskerMaker::generate($errorCorrection, $matrix);
+        return $matrixs;
     }
 
     private static function interleavedCodeword($dataCodewords, Level $level, ErrorCorrection $errorCorrection)
