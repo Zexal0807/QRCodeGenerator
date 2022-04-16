47444eb3bf28dbc00cadc3610dfb0fc03b143776
Zexal0807 <gallinar00@gmail.com>
2022-04-16 16:25:05 +0200

    Generate all matrix masker

--- lib/MaskerMaker.php
+++ lib/MaskerMaker.php
@@ -1,20 +1,18 @@
 <?php
+require_once(dirname(__FILE__) . './matrix/Matrix.php');
 
 class MaskerMaker
 {
-    public static function generate(ErrorCorrection $errorCorrection, $data)
+    public static function generate(ErrorCorrection $errorCorrection, Matrix $matrix)
     {
         $matrixs = [];
-        for ($i = 0; $i < 8; $i++) {
-            $matrixs[$i] = MaskerMaker::generateMatrix($i, $errorCorrection, $data);
-        }
-        //$matrixs[8] = $data;
-
         $penalty = [0, 0, 0, 0, 0, 0, 0, 0];
 
         for ($i = 0; $i < 8; $i++) {
+            $matrixs[$i] = MaskerMaker::generateMatrix($i, $errorCorrection, $matrix);
             $penalty[$i] = MaskerMaker::calcPenalty($matrixs[$i]);
         }
+
         $min = 0;
         for ($i = 1; $i < 8; $i++) {
             if ($penalty[$i] < $penalty[$min]) {
@@ -25,27 +23,134 @@ class MaskerMaker
         return $matrixs[$min];
     }
 
-    private static function color($cell)
+    private static function generateMatrix($mask, ErrorCorrection $errorCorrection, Matrix $data)
     {
-        if ($cell == "D")
-            return "1";
-        if (strlen($cell) == 1)
-            return $cell;
-        return substr($cell, -1);
+        $matrix = new Matrix($data->getSize());
+        for ($y = 0; $y < $data->getSize(); $y++) {
+            for ($x = 0; $x < $data->getSize(); $x++) {
+                if ($data->getCell($y, $x)->isDataCell()) {
+                    $matrix->getCell($y, $x)->setBit(MaskerMaker::{"mask" . $mask}($y, $x, $data->getCell($y, $x)));
+                } else {
+                    $matrix->getCell($y, $x)->setBit($data->getCell($y, $x)->getBit());
+                }
+            }
+        }
+
+        $informationBit = $errorCorrection->getInformationBitOfMask($mask);
+        $mx = $data->getSize() - 1;
+
+        // Set information bits
+        $matrix->setCell(8, 0, "I" . $informationBit[0]);
+        $matrix->setCell(8, 1, "I" . $informationBit[1]);
+        $matrix->setCell(8, 2, "I" . $informationBit[2]);
+        $matrix->setCell(8, 3, "I" . $informationBit[3]);
+        $matrix->setCell(8, 4, "I" . $informationBit[4]);
+        $matrix->setCell(8, 5, "I" . $informationBit[5]);
+        $matrix->setCell(8, 7, "I" . $informationBit[6]);
+        $matrix->setCell(8, 8, "I" . $informationBit[7]);
+        $matrix->setCell(7, 8, "I" . $informationBit[8]);
+        $matrix->setCell(5, 8, "I" . $informationBit[9]);
+        $matrix->setCell(4, 8, "I" . $informationBit[10]);
+        $matrix->setCell(3, 8, "I" . $informationBit[11]);
+        $matrix->setCell(2, 8, "I" . $informationBit[12]);
+        $matrix->setCell(1, 8, "I" . $informationBit[13]);
+        $matrix->setCell(0, 8, "I" . $informationBit[14]);
+
+        $matrix->setCell($mx, 8, "I" . $informationBit[0]);
+        $matrix->setCell($mx - 1, 8, "I" . $informationBit[1]);
+        $matrix->setCell($mx - 2, 8, "I" . $informationBit[2]);
+        $matrix->setCell($mx - 3, 8, "I" . $informationBit[3]);
+        $matrix->setCell($mx - 4, 8, "I" . $informationBit[4]);
+        $matrix->setCell($mx - 5, 8, "I" . $informationBit[5]);
+        $matrix->setCell($mx - 6, 8, "I" . $informationBit[6]);
+        $matrix->setCell(8, $mx - 7, "I" . $informationBit[7]);
+        $matrix->setCell(8, $mx - 6, "I" . $informationBit[8]);
+        $matrix->setCell(8, $mx - 5, "I" . $informationBit[9]);
+        $matrix->setCell(8, $mx - 4, "I" . $informationBit[10]);
+        $matrix->setCell(8, $mx - 3, "I" . $informationBit[11]);
+        $matrix->setCell(8, $mx - 2, "I" . $informationBit[12]);
+        $matrix->setCell(8, $mx - 1, "I" . $informationBit[13]);
+        $matrix->setCell(8, $mx, "I" . $informationBit[14]);
+
+        return $matrix;
     }
 
-    private static function calcPenalty($matrix)
+    private static function mask0($row, $col, Cell $cell)
+    {
+        if (($row + $col) % 2 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask1($row, $col, Cell $cell)
+    {
+        if ($row  % 2 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask2($row, $col, Cell $cell)
+    {
+        if ($col  % 3 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask3($row, $col, Cell $cell)
+    {
+        if (($row + $col) % 3 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask4($row, $col, Cell $cell)
+    {
+        if ((floor($row / 2) + floor($col / 3)) % 2 ==  0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask5($row, $col, Cell $cell)
+    {
+        if ((($row * $col) % 2) + (($row * $col) % 3) == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask6($row, $col, Cell $cell)
+    {
+        if (((($row * $col) % 2) + (($row * $col) % 3)) % 2 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function mask7($row, $col, Cell $cell)
+    {
+        if (((($row + $col) % 2) + (($row * $col) % 3)) % 2 == 0) {
+            return $cell->getBit() == 0 ? 1 : 0;
+        }
+        return $cell->getBit();
+    }
+
+    private static function calcPenalty(Matrix $matrix)
     {
         $penalty = 0;
 
         //penalty for each group of five or more same-colored modules in a row (or column)
         $counter = 1;
         $color = NULL;
-        for ($x = 0; $x < sizeof($matrix); $x++) {
+        for ($y = 0; $y < $matrix->getSize(); $y++) {
             $counter = 1;
             $color = NULL;
-            for ($y = 0; $y < sizeof($matrix); $y++) {
-                $value = MaskerMaker::color($matrix[$x][$y]);
+            for ($x = 0; $x < $matrix->getSize(); $x++) {
+                $value = $matrix->getCell($y, $x)->getColor();
                 if ($color == $value) {
                     $counter++;
                 } else {
@@ -63,11 +168,11 @@ class MaskerMaker
 
         $counter = 1;
         $color = NULL;
-        for ($x = 0; $x < sizeof($matrix); $x++) {
+        for ($x = 0; $x < $matrix->getSize(); $x++) {
             $counter = 1;
             $color = NULL;
-            for ($y = 0; $y < sizeof($matrix); $y++) {
-                $value = MaskerMaker::color($matrix[$y][$x]);
+            for ($y = 0; $y < $matrix->getSize(); $y++) {
+                $value = $matrix->getCell($y, $x)->getColor();
                 if ($color == $value) {
                     $counter++;
                 } else {
@@ -83,14 +188,13 @@ class MaskerMaker
             }
         }
 
-
         //penalty for each 2x2 area of same-colored modules in the matrix
-        for ($x = 0; $x < sizeof($matrix) - 1; $x++) {
-            for ($y = 0; $y < sizeof($matrix) - 1; $y++) {
-                $nw = MaskerMaker::color($matrix[$x][$y]);
-                $ne = MaskerMaker::color($matrix[$x + 1][$y]);
-                $sw = MaskerMaker::color($matrix[$x][$y + 1]);
-                $se = MaskerMaker::color($matrix[$x + 1][$y + 1]);
+        for ($y = 0; $y < $matrix->getSize() - 1; $y++) {
+            for ($x = 0; $x < $matrix->getSize() - 1; $x++) {
+                $nw = $matrix->getCell($y, $x)->getColor();
+                $ne = $matrix->getCell($y, $x + 1)->getColor();
+                $sw = $matrix->getCell($y + 1, $x)->getColor();
+                $se = $matrix->getCell($y + 1, $x + 1)->getColor();
 
                 if ($nw == $ne && $nw == $sw && $nw == $se) {
                     $penalty += 3;
@@ -98,27 +202,14 @@ class MaskerMaker
             }
         }
 
-
         // large penalty if there are patterns that look similar to the finder patterns
-        $pattern = ["1", "0", "1", "1", "1", "0", "1", "0", "0", "0", "0"];
-        for ($x = 0; $x < sizeof($matrix) - sizeof($pattern); $x++) {
-            for ($y = 0; $y < sizeof($matrix) - sizeof($pattern); $y++) {
+        $pattern = ["BLACK", "WHITE", "BLACK", "BLACK", "BLACK", "WHITE", "BLACK", "WHITE", "WHITE", "WHITE", "WHITE"];
+
+        for ($y = 0; $y < $matrix->getSize() - sizeof($pattern); $y++) {
+            for ($x = 0; $x < $matrix->getSize() - sizeof($pattern); $x++) {
                 $find = true;
                 for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
-                    if (MaskerMaker::color($matrix[$x][$y]) != $pattern[$k]) {
-                        $find = false;
-                    }
-                }
-                if ($find) {
-                    $penalty += 40;
-                }
-            }
-        }
-        for ($x = 0; $x < sizeof($matrix) - sizeof($pattern); $x++) {
-            for ($y = 0; $y < sizeof($matrix) - sizeof($pattern); $y++) {
-                $find = true;
-                for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
-                    if (MaskerMaker::color($matrix[$y][$x]) != $pattern[$k]) {
+                    if ($matrix->getCell($y, $x)->getColor() != $pattern[$k]) {
                         $find = false;
                     }
                 }
@@ -128,25 +219,11 @@ class MaskerMaker
             }
         }
 
-        $pattern = ["0", "0", "0", "0", "1", "0", "1", "1", "1", "0", "1"];
-        for ($x = 0; $x < sizeof($matrix) - sizeof($pattern); $x++) {
-            for ($y = 0; $y < sizeof($matrix) - sizeof($pattern); $y++) {
+        for ($y = 0; $y < $matrix->getSize() - sizeof($pattern); $y++) {
+            for ($x = 0; $x < $matrix->getSize() - sizeof($pattern); $x++) {
                 $find = true;
                 for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
-                    if (MaskerMaker::color($matrix[$x][$y]) != $pattern[$k]) {
-                        $find = false;
-                    }
-                }
-                if ($find) {
-                    $penalty += 40;
-                }
-            }
-        }
-        for ($x = 0; $x < sizeof($matrix) - sizeof($pattern); $x++) {
-            for ($y = 0; $y < sizeof($matrix) - sizeof($pattern); $y++) {
-                $find = true;
-                for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
-                    if (MaskerMaker::color($matrix[$y][$x]) != $pattern[$k]) {
+                    if ($matrix->getCell($x, $y)->getColor() != $pattern[$k]) {
                         $find = false;
                     }
                 }
@@ -156,13 +233,41 @@ class MaskerMaker
             }
         }
 
+        $pattern = ["WHITE", "WHITE", "WHITE", "WHITE", "BLACK", "WHITE", "BLACK", "BLACK", "BLACK", "WHITE", "BLACK"];
+        for ($y = 0; $y < $matrix->getSize() - sizeof($pattern); $y++) {
+            for ($x = 0; $x < $matrix->getSize() - sizeof($pattern); $x++) {
+                $find = true;
+                for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
+                    if ($matrix->getCell($y, $x)->getColor() != $pattern[$k]) {
+                        $find = false;
+                    }
+                }
+                if ($find) {
+                    $penalty += 40;
+                }
+            }
+        }
+
+        for ($y = 0; $y < $matrix->getSize() - sizeof($pattern); $y++) {
+            for ($x = 0; $x < $matrix->getSize() - sizeof($pattern); $x++) {
+                $find = true;
+                for ($k = 0; $k < sizeof($pattern) && $find; $k++) {
+                    if ($matrix->getCell($x, $y)->getColor() != $pattern[$k]) {
+                        $find = false;
+                    }
+                }
+                if ($find) {
+                    $penalty += 40;
+                }
+            }
+        }
 
         //penalty if more than half of the modules are dark or light, with a larger penalty for a larger difference
         $b = 0;
         $w = 0;
-        for ($x = 0; $x < sizeof($matrix); $x++) {
-            for ($y = 0; $y < sizeof($matrix); $y++) {
-                if (MaskerMaker::color($matrix[$x][$y]) == "0") {
+        for ($y = 0; $y < $matrix->getSize() - sizeof($pattern); $y++) {
+            for ($x = 0; $x < $matrix->getSize() - sizeof($pattern); $x++) {
+                if ($matrix->getCell($x, $y)->getColor() == "WHITE") {
                     $w++;
                 } else {
                     $b++;
@@ -178,122 +283,5 @@ class MaskerMaker
         $penalty += min(abs($prev - 50) / 5, abs($next - 50) / 5) * 10;
 
         return $penalty;
-    }
-
-    private static function generateMatrix($mask, ErrorCorrection $errorCorrection, $data)
-    {
-        $matrix = [];
-        for ($y = 0; $y < sizeof($data); $y++) {
-            for ($x = 0; $x < sizeof($data); $x++) {
-                if (in_array($data[$y][$x], ["0", "1"])) {
-                    $matrix[$y][$x] = MaskerMaker::{"mask" . $mask}($y, $x, $data[$y][$x]);
-                } else {
-                    $matrix[$y][$x] = $data[$y][$x];
-                }
-            }
-        }
-
-        $informationBit = $errorCorrection->getInformationBitOfMask($mask);
-        $mx = sizeof($data) - 1;
-        //set 
-        $matrix[8][0] = "I" . $informationBit[0];
-        $matrix[8][1] = "I" . $informationBit[1];
-        $matrix[8][2] = "I" . $informationBit[2];
-        $matrix[8][3] = "I" . $informationBit[3];
-        $matrix[8][4] = "I" . $informationBit[4];
-        $matrix[8][5] = "I" . $informationBit[5];
-        $matrix[8][7] = "I" . $informationBit[6];
-        $matrix[8][8] = "I" . $informationBit[7];
-        $matrix[7][8] = "I" . $informationBit[8];
-        $matrix[5][8] = "I" . $informationBit[9];
-        $matrix[4][8] = "I" . $informationBit[10];
-        $matrix[3][8] = "I" . $informationBit[11];
-        $matrix[2][8] = "I" . $informationBit[12];
-        $matrix[1][8] = "I" . $informationBit[13];
-        $matrix[0][8] = "I" . $informationBit[14];
-
-        $matrix[$mx][8] = "I" . $informationBit[0];
-        $matrix[$mx - 1][8] = "I" . $informationBit[1];
-        $matrix[$mx - 2][8] = "I" . $informationBit[2];
-        $matrix[$mx - 3][8] = "I" . $informationBit[3];
-        $matrix[$mx - 4][8] = "I" . $informationBit[4];
-        $matrix[$mx - 5][8] = "I" . $informationBit[5];
-        $matrix[$mx - 6][8] = "I" . $informationBit[6];
-        $matrix[8][$mx - 7] = "I" . $informationBit[7];
-        $matrix[8][$mx - 6] = "I" . $informationBit[8];
-        $matrix[8][$mx - 5] = "I" . $informationBit[9];
-        $matrix[8][$mx - 4] = "I" . $informationBit[10];
-        $matrix[8][$mx - 3] = "I" . $informationBit[11];
-        $matrix[8][$mx - 2] = "I" . $informationBit[12];
-        $matrix[8][$mx - 1] = "I" . $informationBit[13];
-        $matrix[8][$mx] = "I" . $informationBit[14];
-
-
-        return $matrix;
-    }
-
-    private static function mask0($row, $col, $value)
-    {
-        if (($row + $col) % 2 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask1($row, $col, $value)
-    {
-        if ($row  % 2 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask2($row, $col, $value)
-    {
-        if ($col  % 3 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask3($row, $col, $value)
-    {
-        if (($row + $col) % 3 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask4($row, $col, $value)
-    {
-        if ((floor($row / 2) + floor($col / 3)) % 2 ==  0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask5($row, $col, $value)
-    {
-        if ((($row * $col) % 2) + (($row * $col) % 3) == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask6($row, $col, $value)
-    {
-        if (((($row * $col) % 2) + (($row * $col) % 3)) % 2 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
-    }
-    private static function mask7($row, $col, $value)
-    {
-        if (((($row + $col) % 2) + (($row * $col) % 3)) % 2 == 0) {
-            return $value == "0" ? "01" : "10";
-        }
-
-        return $value;
     }
 }
