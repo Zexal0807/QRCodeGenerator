5cceabdc102151389ec25e248e381949f3e7ac61
Zexal0807 <gallinar00@gmail.com>
2022-04-17 11:04:21 +0200

    Implement byte encoding

--- lib/encode/ByteModeEncoding.php
+++ lib/encode/ByteModeEncoding.php
@@ -1,14 +1,18 @@
 <?php
 require_once(dirname(__FILE__) . './Encoding.php');
 
-
 class ByteModeEncoding extends Encoding
 {
     public function encode($data)
     {
         $data = utf8_encode($data);
 
-        // TODO: implementare
-        return $data;
+        $data = str_split($data, 1);
+
+        $data = array_map(function ($el) {
+            return sprintf("%08b", ord($el));
+        }, $data);
+
+        return implode("", $data);
     }
 }
