351a9fc1ba8f18cd444470c778ff835218d53d47
Zexal0807 <gallinar00@gmail.com>
2022-04-17 11:04:49 +0200

    Add format of different content type

--- lib/matrix/Matrix.php
+++ lib/matrix/Matrix.php
@@ -35,13 +35,27 @@ class Matrix
     }
 
     // TODO: Implement logo space using errorCorrection perc
+    // TODO: Add different type of content
+    /*
+        url (link o altri) : "https://www.youtube.com/watch?v=3BhdrSP4v68&ab_channel=ShevanFreeman"
+        email: "mailto:gallinar00@gmail.com?subject=gallinar00@gmail.com&body=asasas"
+        phone number : "tel:3312831040"
+        SMS : "SMSTO:3312831040:yujjt"
+        position : "https://maps.google.com/local?q=51.049259,13.73836"
+        WIFIPSW : "WIFI:S:name;T:WPA;P:password;;"
+                  "WIFI:S:name;T:nopass;P:password;;"
+                  "WIFI:S:name;T:WEP;P:password;;"
+        EVENT : "BEGIN:VEVENT\nSUMMARY:sddasdas\nLOCATION:asdasd\nDTSTART:20220416T191400\nDTEND:20220416T191400\nEND:VEVENT\n"
+
+    */
+
     public function print()
     {
         $html = "<table>";
         for ($i = 0; $i < sizeof($this->data); $i++) {
             $html .= "<tr>";
             for ($j = 0; $j < sizeof($this->data); $j++) {
-                $html .= '<td color="' . $this->data[$i][$j]->getColor() . '">' . substr($this->data[$i][$j]->getColor(), 0, 1) . '</td>';
+                $html .= '<td color="' . $this->data[$i][$j]->getColor() . '"></td>';
             }
             $html .= "</tr>";
         }
