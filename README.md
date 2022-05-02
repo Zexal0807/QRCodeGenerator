087dfee16fbaf3014b2c2f9a6227a2fd3a8dfba6
Zexal0807 <gallinar00@gmail.com>
2022-05-02 18:34:01 +0200

    Improve doc

--- README.md
+++ README.md
@@ -12,3 +12,53 @@ Guida : [https://www.thonky.com/qr-code-tutorial/introduction](https://www.thonk
 - Create specific method for different content type
 - Implement logo
 - Implement all level size
+
+## How to use
+
+```php
+require_once('./lib/QRCode.php');
+
+$data = "tel:3333333333";
+$errorCorrection = ErrorCorrection::$CORRECTION_M;
+
+$matrix = QRCode::create($data, $errorCorrection);
+
+echo $matrix->print();
+
+```
+
+`print` method return a html string where there's a table where each cell a attribute color
+
+So add style for see QRCode
+
+```css
+td[color="BLACK"] {
+  color: white;
+  background-color: black;
+}
+
+td[color="WHITE"] {
+  background-color: white;
+}
+
+td[color="RESERVED"] {
+  background-color: blue;
+}
+
+td {
+  border-collapse: collapse;
+}
+
+table {
+  width: 500;
+  height: 500;
+  margin: 15px;
+  border-collapse: collapse;
+}
+```
+
+Reserved cell will bge used in future, for example for add image
+
+Here the result
+
+![Result](./doc/example.png)
--- index.php
+++ index.php
@@ -1,61 +1,26 @@
 <head>
     <style>
-        td[color="F1"],
-        td[color="A1"],
-        td[color="T1"],
-        td[color="D"],
-        td[color="R1"],
-        td[color="1"],
-        td[color="01"],
-        td[color="S1"],
         td[color="BLACK"] {
             color: white;
             background-color: black;
         }
 
-        td[color="F0"],
-        td[color="A0"],
-        td[color="T0"],
-        td[color="R0"],
-        td[color="0"],
-        td[color="10"],
-        td[color="S0"],
         td[color="WHITE"] {
             background-color: white;
         }
 
-        td[color="I1"] {
-            background-color: gray;
-            background-color: black;
-            color: white;
-        }
-
-        td[color="I0"] {
-            background-color: #404040;
-            background-color: white;
-        }
-
-        td[color="R"],
         td[color="RESERVED"] {
             background-color: blue;
         }
 
         td {
-            /* border: 1px solid black; */
             border-collapse: collapse;
-            background-color: #ff0000;
-        }
-
-        div {
-            display: flex;
-            flex-wrap: wrap;
         }
 
         table {
             width: 500;
             height: 500;
             margin: 15px;
-            /* border: 1px solid black; */
             border-collapse: collapse;
         }
     </style>
